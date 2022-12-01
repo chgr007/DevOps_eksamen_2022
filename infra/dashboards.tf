47878a84d8b7a2f9f8db7a7c288c5216c1323e77
Christian Gregersen <christian@gregersen.eu>
2022-12-01 15:00:20 +0100

    Bestemt meg for hvordan jeg skal regne gjennomsnitt. Brukt alt for mye tid på å gruble på den der 😂 Hadde vært fint om Micrometer kunne dokumentert bedre de forskjellige målene man får. Må bare stole på at forum-poster vet best her.

--- infra/dashboards.tf
+++ infra/dashboards.tf
@@ -88,11 +88,11 @@ resource "aws_cloudwatch_dashboard" "main" {
       "height": 6,
       "properties": {
         "metrics": [
-          ["${var.candidate_id}", "checkout_t.avg", "exception", "None", "method", "POST", "uri", "/cart/checkout", "outcome", "SUCCESS", "status", "200" ]
+          ["${var.candidate_id}", "checkout_t.max", "exception", "None", "method", "POST", "uri", "/cart/checkout", "outcome", "SUCCESS", "status", "200" ]
         ],
         "view": "timeSeries",
         "period": 60,
-        "stat": "Maximum",
+        "stat": "Average",
         "region": "eu-west-1",
         "title": "Average time of checkout request"
       }
