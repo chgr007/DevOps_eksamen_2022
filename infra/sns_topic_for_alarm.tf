9000615cdafccea55a7a468a998ba6a14237f881
Christian Gregersen <christian@gregersen.eu>
2022-12-01 12:02:32 +0100

    Alarmer funker lokalt

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,22 @@
+
+# En CloudWatch Alarm som løses ut dersom antall handlekurver over tre repeternde perioder, på fem minutter, overstiger verdien 5
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "bank-sum-must-be-0"
+  namespace                 = "1012"
+  metric_name               = "cart_count.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "5"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of shopping carts over three repeating periods of five minutes, exceeds 5"
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
